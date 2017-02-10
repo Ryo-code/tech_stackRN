56344a348b8950af2b4e1216b872bbe5170feca9
Ryo <kanadajinlee@yahoo.ca>
2017-02-09 20:45:05 -0500

    Just made the list properly render

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <Provider store={createStore(reducers)}> 
       {/* Provider can only have ONE child component */}
-      <View>
+      <View style={{flex: 1}}>
         <Header headerText="Tech Stack"/>
         <LibraryList/>
       </View>
@@ -25,14 +25,15 @@ export default App;
 1) When the app first boots up, redux creates a new store using the "libraries" reducer. 
 2) The instance the store is created, it runs the reducer one time. 
   ╰─➤ This yields a piece of state called "libraries: []" (an array of objects).
-3) When it runs, the reducer returns that array of objects (each object 
-represents an object that we'll render on the screen).
-4) Once the store is created, it is passed as props to the Provider (where it'll 
+3) When the Reducer runs, it returns that array of objects (each object 
+  represents an object that we'll render on the screen).
+4) Once the Store is created, it is passed as props to the Provider (where it'll 
   remain for the lifespan of this app).
   ╰─➤The Provider is a react component that aids in communication between react & redux.
 5) The App component is rendered to the screen, which in turn renders the LibraryList component.
-6) The instance the LibraryList component is about to render on the screen, the Connect 
-  function activates, and reaches back into the Provider and grabs whatever the state is.
+6) The instant the LibraryList component is about to render on the screen, the Connect 
+  function activates, and grabs the state from the Provider.
+  ╰─➤The Provider gives the state that's in the Store.
 7) The Connect helper then calls 'mapStateToProps' and whatever object gets returned (ex. the state),
   will show up in LibraryList as props
 
